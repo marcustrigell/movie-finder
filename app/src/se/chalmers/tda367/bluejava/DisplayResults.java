990bed1f4b7b92612faa620dd2c065484c56c1ce
Marcus Trigell <leppmannen@gmail.com>
2014-03-24 15:08:53 +0100

    new Class-file

---
+++ app/src/se/chalmers/tda367/bluejava/DisplayResults.java
@@ -0,0 +1,11 @@
+package se.chalmers.tda367.bluejava;
+
+/**
+ * Created by marcus on 2014-03-24.
+ */
+public class DisplayResults {
+    public void onCreate() {
+        super.onCreate();
+        
+    }
+}
