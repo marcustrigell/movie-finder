43ac6987c5bc0fd24ae8e058051b0cd7fa5bd2b2
Marcus Trigell <leppmannen@gmail.com>
2014-03-25 20:23:33 +0100

    created interface for sorting

---
+++ app/src/se/chalmers/tda367/bluejava/ISort.java
@@ -0,0 +1,18 @@
+package se.chalmers.tda367.bluejava;
+
+import java.util.List;
+
+/**
+ * Created by marcus on 2014-03-25.
+ *
+ * Interface for sorting hits in the
+ * DisplayResultsActivity-class.
+ *
+ * @author Marcus Trigell
+ *
+ */
+public interface ISort {
+
+    public void sort(List<Movie> list);
+
+}
