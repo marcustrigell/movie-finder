1a426e777a7337c237ff6918d6c49a9415a813dd
Axel Niklasson <axel.niklasson@live.com>
2014-03-27 15:08:03 +0100

    Detailed view gets info from Movie.

--- app/src/se/chalmers/tda367/bluejava/DisplayMovieActivity.java
+++ app/src/se/chalmers/tda367/bluejava/DisplayMovieActivity.java
@@ -6,14 +6,28 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class DisplayMovieActivity extends Activity {
+
+    private Movie movie;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.display_movie_activity);
 
         Intent intent = getIntent();
-        Movie movie = (Movie)intent.getParcelableExtra("key1");
+        movie = (Movie)intent.getParcelableExtra("key1");
 
+        loadInfo();
+    }
+
+    public void loadInfo() {
         TextView titleTextView = (TextView) findViewById(R.id.title);
+        TextView descriptionTextView = (TextView) findViewById(R.id.description);
+        TextView ratingTextView = (TextView) findViewById(R.id.rating);
+        TextView voteCountTextView = (TextView) findViewById(R.id.vote_count);
+
         titleTextView.setText(movie.getTitle());
+        descriptionTextView.setText("[Description of the movie");
+        ratingTextView.setText("[Rating of the movie]");
+        voteCountTextView.setText("[Vote count of the movie");
     }
 }
