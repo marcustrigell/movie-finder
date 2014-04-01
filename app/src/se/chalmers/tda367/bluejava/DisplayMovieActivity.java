798ddfe9a4f09f31b9dac7bd7c3c6521320e765c
Axel Niklasson <axel.niklasson@live.com>
2014-04-01 11:40:42 +0200

    layout changes in detailed view.

--- app/src/se/chalmers/tda367/bluejava/DisplayMovieActivity.java
+++ app/src/se/chalmers/tda367/bluejava/DisplayMovieActivity.java
@@ -25,26 +25,28 @@ public class DisplayMovieActivity extends Activity {
     }
 
     public void loadInfo() {
+        //Finding the fields that is to be set to values
         TextView titleTextView = (TextView) findViewById(R.id.title);
+        ImageView posterImageView = (ImageView) findViewById(R.id.posterImageView);
         TextView descriptionTextView = (TextView) findViewById(R.id.description);
+        RatingBar ratingBar = (RatingBar) findViewById(R.id.ratingBar);
         TextView releaseYearTextView = (TextView) findViewById(R.id.release_year);
         TextView popularityTextView = (TextView) findViewById(R.id.popularity);
 
         //Inserting the image in the poster image view
-        ImageView posterImageView = (ImageView) findViewById(R.id.posterImageView);
         String url = movieApi.getThumbnailURL(movie.getPosterPath());
         Picasso.with(this).load(url).into(posterImageView);
 
         //Setting the rating to the rating bar
-        RatingBar ratingBar = (RatingBar) findViewById(R.id.ratingBar);
         ratingBar.setRating(Float.parseFloat(movie.getRating()) / 2);
 
         //Rounding the popularity
         double popularityRounded = Double.parseDouble(movie.getPopularity());
         popularityRounded = Math.round(popularityRounded);
 
+        //Setting the strings to values
         titleTextView.setText(movie.getTitle());
-        descriptionTextView.setText("[Description of the movie]");
+        descriptionTextView.setText("[Tagline]");
         releaseYearTextView.setText("Release year: " + movie.getReleaseYear());
         popularityTextView.setText("Popularity: " + popularityRounded);
     }
