2197cb86524a28ab7d1247f09b127e569c2ad2ad
Marcus Trigell <leppmannen@gmail.com>
2014-04-10 11:16:53 +0200

    changes in SortByTitle-test

--- app/src/se/chalmers/tda367/bluejava/test/SortByTitleTest.java
+++ app/src/se/chalmers/tda367/bluejava/test/SortByTitleTest.java
@@ -2,6 +2,9 @@ package se.chalmers.tda367.bluejava.test;
 
 import junit.framework.TestCase;
 import org.json.JSONObject;
+import se.chalmers.tda367.bluejava.ISort;
+import se.chalmers.tda367.bluejava.Movie;
+import se.chalmers.tda367.bluejava.SortByTitle;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -21,23 +24,26 @@ public class SortByTitleTest extends TestCase {
 
     public void testSort() throws Exception {
 
-        //Scanner in = new Scanner(new FileReader("json.txt"));
-        //String json = in.nextLine();
+        Scanner in = new Scanner(new FileReader("json.txt"));
+        String json = "";
+        while(in.hasNext()) {
+            json = json + "\n" + in.nextLine();
+        }
 
-        //List<Movie> list = Movie.jsonToListOfMovies(json);
+        List<Movie> list = Movie.jsonToListOfMovies(json);
 
-        //List<String> titles = new ArrayList<String>();
-        //for(Movie movie : list) {
-        //    titles.add(movie.getTitle()); // TODO
-        //}
+        List<String> titles = new ArrayList<String>();
+        for(Movie movie : list) {
+            titles.add(movie.getTitle()); // TODO
+        }
 
-        //ISort sortMethod = new SortByTitle();
-        //list = sortMethod.sort(list);
+        ISort sortMethod = new SortByTitle();
+        list = sortMethod.sort(list);
 
-        //int index = 0;
-        //for(Movie movie : list) {
-        //    assertTrue(movie.getTitle.equals(titles[index])); // TODO
-        //}
+        int index = 0;
+        for(Movie movie : list) {
+            assertTrue(movie.getTitle().equals(titles.get(index))); // TODO
+        }
 
     }
 }
