4f57f61b0d3c9a8b780aafab010b98b04e570367
Marcus Trigell <leppmannen@gmail.com>
2014-05-01 19:27:32 +0200

    created testclasses

---
+++ Tests/src/se/chalmers/tda367/bluejava/HttpHandlerTest.java
@@ -0,0 +1,18 @@
+package se.chalmers.tda367.bluejava;
+
+import junit.framework.TestCase;
+
+public class HttpHandlerTest extends TestCase {
+
+    public void testGet() throws Exception {
+
+    }
+
+    public void testGetData() throws Exception {
+
+    }
+
+    public void testGetAndroidHttpClient() throws Exception {
+
+    }
+}
