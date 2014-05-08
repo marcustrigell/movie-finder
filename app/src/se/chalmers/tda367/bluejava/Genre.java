d03b9bc826c2123bfae3a3329e083eb4a2df406d
Tobias Andersen <j.tobiasandersen@gmail.com>
2014-05-09 00:35:36 +0200

    Added Genre class

---
+++ app/src/se/chalmers/tda367/bluejava/Genre.java
@@ -0,0 +1,77 @@
+package se.chalmers.tda367.bluejava;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Genre implements Parcelable {
+
+    private final int ID;
+
+    private final String name;
+
+    public Genre(JSONObject jsonObject) throws JSONException {
+        this.ID = jsonObject.getInt("id");
+        this.name = jsonObject.getString("name");
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static List<Genre> jsonToListOfGenres(JSONArray jsonArray) {
+
+        try {
+            List<Genre> genres = new ArrayList<Genre>();
+
+            for (int i = 0; i < jsonArray.length(); ++i) {
+                JSONObject jsonGenre = jsonArray.getJSONObject(i);
+                Genre genre = new Genre(jsonGenre);
+                genres.add(genre);
+            }
+
+            return genres;
+
+        } catch (JSONException e) {
+            throw new RuntimeException("Actor.java jsonToListOfActors error");
+        }
+    }
+
+    protected Genre(Parcel in) {
+        ID = in.readInt();
+        name = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(ID);
+        dest.writeString(name);
+    }
+
+    public static final Parcelable.Creator<Genre> CREATOR = new Parcelable.Creator<Genre>() {
+        @Override
+        public Genre createFromParcel(Parcel in) {
+            return new Genre(in);
+        }
+
+        @Override
+        public Genre[] newArray(int size) {
+            return new Genre[size];
+        }
+    };
+}
+
