be4c53f6a54b342fec730aa2582ffd49336db6cd
Axel Niklasson <axel.niklasson@live.com>
2014-05-08 23:00:59 +0200

    Removed old module for testing.

--- Tests/gen/se/chalmers/tda367/bluejava/tests/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package se.chalmers.tda367.bluejava.tests;
-
-/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
-public final class Manifest {
-}
