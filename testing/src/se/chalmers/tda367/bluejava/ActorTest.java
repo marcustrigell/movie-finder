f52339e3306339e02aae8721c21b3c562b34d27d
Axel Niklasson <axel.niklasson@live.com>
2014-05-08 20:27:58 +0200

    Added 3 more test classes.

---
+++ testing/src/se/chalmers/tda367/bluejava/ActorTest.java
@@ -0,0 +1,37 @@
+package se.chalmers.tda367.bluejava;
+
+import junit.framework.TestCase;
+import org.junit.*;
+
+/**
+ * Created by axelniklasson on 2014-05-08.
+ */
+public class ActorTest extends TestCase {
+
+    private static Actor actor;
+
+    @BeforeClass
+    public static void initialize() {
+    }
+
+    @Test
+    public void testGetCastID() {
+
+    }
+
+    @Test
+    public void testGetCharacter() {
+
+    }
+
+    @Test
+    public void testGetCreditID() {
+
+    }
+
+    @Test
+    public void testGetOrder() {
+
+    }
+
+}
---
+++ testing/src/se/chalmers/tda367/bluejava/CrewMemberTest.java
@@ -0,0 +1,33 @@
+package se.chalmers.tda367.bluejava;
+
+import junit.framework.TestCase;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Created by axelniklasson on 2014-05-08.
+ */
+public class CrewMemberTest extends TestCase {
+
+    private CrewMember crewMember;
+
+    @BeforeClass
+    public static void initialize() {
+
+    }
+
+    @Test
+    public void testGetCreditID() {
+
+    }
+
+    @Test
+    public void testGetDepartment() {
+
+    }
+
+    @Test
+    public void testGetOrder() {
+
+    }
+}
