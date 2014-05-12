4ffdfe036e913dfa66e34f86bfcccae1901329c6
Tobias Andersen <j.tobiasandersen@gmail.com>
2014-05-12 14:02:15 +0200

    Removed imports

--- app/src/se/chalmers/tda367/bluejava/adapters/CastTabArrayAdapter.java
+++ app/src/se/chalmers/tda367/bluejava/adapters/CastTabArrayAdapter.java
@@ -2,17 +2,13 @@ package se.chalmers.tda367.bluejava.adapters;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.squareup.picasso.Picasso;
 import se.chalmers.tda367.bluejava.R;
-import se.chalmers.tda367.bluejava.activities.DisplayMovieActivity;
 import se.chalmers.tda367.bluejava.models.Actor;
 
 import java.util.List;
