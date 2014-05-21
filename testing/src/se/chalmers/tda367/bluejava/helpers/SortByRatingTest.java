723a0dbe850efeb31b20eb93cf9727f4d780a775
Marcus Trigell <leppmannen@gmail.com>
2014-05-21 10:56:49 +0200

    completed sorting tests

--- testing/src/se/chalmers/tda367/bluejava/helpers/SortByRatingTest.java
+++ testing/src/se/chalmers/tda367/bluejava/helpers/SortByRatingTest.java
@@ -1,10 +1,36 @@
 package se.chalmers.tda367.bluejava.helpers;
 
 import junit.framework.TestCase;
+import org.junit.Test;
+import se.chalmers.tda367.bluejava.interfaces.SortMethod;
+import se.chalmers.tda367.bluejava.models.Movie;
+
+import java.io.FileReader;
+import java.util.List;
+import java.util.Scanner;
 
 public class SortByRatingTest extends TestCase {
 
-    public void testSort() throws Exception {
+    private List<Movie> sortedList, unsortedList, correctList;
+    private SortMethod sortMethod;
+    private String correct, unsorted;
 
+    @Test
+    public void testSort() throws Exception {
+        Scanner sc = new Scanner(new FileReader("/Users/axelniklasson/dev/blue-java/testing/resources/unsorted.txt"));
+        while(sc.hasNextLine()) {
+            unsorted += sc.nextLine();
+        }
+        unsortedList = Movie.jsonToListOfMovies(unsorted);
+        sc = new Scanner(new FileReader("/Users/axelniklasson/dev/blue-java/testing/resources/rating_sorted.txt"));
+        while(sc.hasNextLine()) {
+            correct += sc.nextLine();
+        }
+        correctList = Movie.jsonToListOfMovies(correct);
+        sortMethod = new SortByTitle();
+        sortedList = sortMethod.sort(unsortedList);
+        for(Movie movie : sortedList) {
+            assertEquals(correctList, sortedList);
+        }
     }
 }
